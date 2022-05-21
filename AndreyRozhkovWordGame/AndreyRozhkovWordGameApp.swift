36896ff592defa7009efa7ebcd425e6eb4fb4fe1
Andrey Rozhkov <an.rozhkov@corp.mail.ru>
2022-05-21 09:55:54 +0200

    Initial Commit

---
+++ AndreyRozhkovWordGame/AndreyRozhkovWordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndreyRozhkovWordGameApp.swift
+//  AndreyRozhkovWordGame
+//
+//  Created by an.rozhkov on 21.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AndreyRozhkovWordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AndreyRozhkovWordGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AndreyRozhkovWordGame
+//
+//  Created by an.rozhkov on 21.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
